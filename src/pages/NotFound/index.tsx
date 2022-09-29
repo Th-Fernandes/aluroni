963956563aaf168c4044e62f6d8cf3ea9ca5c34f
Thiago Fernandes <tjfctigo@gmail.com>
2022-09-29 11:25:53 -0300

    notFound page: voltar pagina anterior

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,14 +2,22 @@ import {ReactComponent as NotFoundBg} from 'assets/not_found.svg';
 import classNames from 'classnames';
 import styles from './NotFound.module.scss';
 import stylesTheme from 'styles/Theme.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div className={classNames({
       [styles.container]: true,
       [stylesTheme.container]: true
     })}>
-    
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>
+          {'< voltar'}
+        </button>
+      </div>
+
       <NotFoundBg />
     </div>  
   );
