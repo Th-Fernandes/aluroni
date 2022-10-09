faa2648d75a6be1e0c986e47e0371a5ec0398a3b
Thiago Fernandes <tjfctigo@gmail.com>
2022-10-09 08:52:44 -0300

    global style: removing local cursor style

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
-import './index.css';
 import Router from 'routes';
-
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
